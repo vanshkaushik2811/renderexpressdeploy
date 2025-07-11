7809bb15741608b4df6b1e7cc520b7d5545f7989
Vansh Kaushik <vanshkaushik2811@gmail.com>
2025-07-11 05:30:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,70 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-
 var app = express();
 app.set("view engine","ejs");
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
-var items = [];
-var example="working";
+app.use(express.json());
+
+
+var todos = [];
+
 app.get("/",function(req,res){
-    res.render("list",{ejes : items })
+    res.render("list",{todos: todos});
 });
-app.post("/",function(req, res){
-    var item = req.body.ele1;
-    items.push(item);
+
+
+app.post("/add",function(req, res){
+    var todoText = req.body.todoText;
+    var priority = req.body.priority || "medium";
+    
+    if (todoText && todoText.trim() !== "") {
+        var newTodo = {
+            id: Date.now(),
+            text: todoText.trim(),
+            priority: priority,
+            completed: false,
+            createdAt: new Date()
+        };
+        todos.push(newTodo);
+    }
     res.redirect("/");
 });
+
+
+app.post("/delete/:id", function(req, res) {
+    var todoId = parseInt(req.params.id);
+    todos = todos.filter(todo => todo.id !== todoId);
+    res.redirect("/");
+});
+
+
+app.post("/edit/:id", function(req, res) {
+    var todoId = parseInt(req.params.id);
+    var newText = req.body.newText;
+    var newPriority = req.body.newPriority;
+    
+    if (newText && newText.trim() !== "") {
+        var todoIndex = todos.findIndex(todo => todo.id === todoId);
+        if (todoIndex !== -1) {
+            todos[todoIndex].text = newText.trim();
+            todos[todoIndex].priority = newPriority;
+        }
+    }
+    res.redirect("/");
+});
+
+app.post("/toggle/:id", function(req, res) {
+    var todoId = parseInt(req.params.id);
+    var todoIndex = todos.findIndex(todo => todo.id === todoId);
+    if (todoIndex !== -1) {
+        todos[todoIndex].completed = !todos[todoIndex].completed;
+    }
+    res.redirect("/");
+});
+
 app.listen(4000,function(){
     console.log("Server Started");
-}
-);
+});
+
